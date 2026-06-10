6edefe30bb0a6768e319cb5441be7ef5d7fd3a87
croznfaith <tg.srchub@gmail.com>
2026-06-10 21:58:23 +0530

    Create main.h

---
+++ wtf/ftw/lop/qeru/wero/qwero/main.h
@@ -0,0 +1,8 @@
+NAME=[SRC HUB]
+KEY={"SRCHUB"200"10-06-2027"FREE-KEY"}
+//here you can add new key
+BotToken={"8922436419:AAGu7n_iIO6-lImvIjP7iFyMvEewD6tPsdw"}
+Userid={"5730398152"}
+DeveloperName={"XThrlen"}
+ChannelName={"SRC HUB"}
+ToolVersion={"4.5.1}
